8528e5601172aca9f9f300bc6572a9fec7e3fe05
Rahul Patel <rahuloct625@gmail.com>
2026-03-19 15:02:05 +0530

    up

--- src/Components/Pages/Dashboard.jsx
+++ src/Components/Pages/Dashboard.jsx
@@ -220,6 +220,7 @@ export default function Dashboard() {
         </div>
 
         <nav className="sidebar-menu">
+          <a onClick={() => navigate("/")}>🏠 Home</a>
           <a className="active">🏠 Dashboard</a>
           <a onClick={() => navigate("/my-courses")}>📚 My Courses</a>
           <a>📊 Progress</a>
