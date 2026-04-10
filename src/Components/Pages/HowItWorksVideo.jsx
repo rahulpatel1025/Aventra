1caad5e9cda72d7d109f6e62d350085c4923b710
Rahul Patel <rahuloct625@gmail.com>
2026-04-10 11:51:57 +0530

    thumnail

--- src/Components/Pages/HowItWorksVideo.jsx
+++ src/Components/Pages/HowItWorksVideo.jsx
@@ -44,8 +44,8 @@ export default function HowItWorksVideo() {
                         }}>
                             { <video 
                                 controls 
-                                poster="/img/video-thumbnail.jpg" // Optional: an image to show before they click play
-                                style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", objectFit: "cover" }}
+                                poster="/img/thumbnail.jpg" // Optional: an image to show before they click play
+                                style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", objectFit: "" }}
                             >
                                 <source src="/video/guide.mp4" type="video/mp4" />
                                 Your browser does not support the video tag.
